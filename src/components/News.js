3b4e60992af126249d1bc041c246bb244453b4d8
Sundaram Sharma <sundaram-sharma@hotmail.com>
2023-02-20 22:09:07 +0530

    Fetching news category wise

--- src/components/News.js
+++ src/components/News.js
@@ -86,8 +86,8 @@ export class News extends Component {
             
         </div>
         <div className='container d-flex justify-content-between'> 
-        <button disabled={this.state.page<=1} type="button" class="btn btn-dark" onClick={this.handlePreviousClick} >&larr; Previous</button>
-        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" class="btn btn-dark"onClick={this.handleNextClick} >Next &rarr;</button>
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick} >&larr; Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark"onClick={this.handleNextClick} >Next &rarr;</button>
         </div>
       </div>
     )
